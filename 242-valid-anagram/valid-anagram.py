4022c4b73f6c1e874951fa0750bcb370967a5429
Meenadevi Ravikumar <meenadeviravikumar@gmail.com>
2026-08-17 14:43:42 +0530

    Time: 18 ms (19.25%) | Memory: 20.4 MB (10.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         x=sorted(s)
-        # m=len(x)
         y=sorted(t)
-        # n=len(y)
-        if x==y:
-            return True
-        else:
-            return False
+        return x==y
+
 
                 
