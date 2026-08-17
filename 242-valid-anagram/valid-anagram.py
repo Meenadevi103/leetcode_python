e0ea0dd32b5912afb42f0aa7941ddee1e69fce18
Meenadevi Ravikumar <meenadeviravikumar@gmail.com>
2026-08-17 15:15:54 +0530

    Time: 3 ms (98.39%) | Memory: 19.4 MB (75.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,20 +3,26 @@ class Solution:
         # x=sorted(s)
         # y=sorted(t)
         # return x==y  
-        dict1={}
-        dict2={}
+        # dict1={}
+        # dict2={}
 
-        for i in s:
-            if i not in dict1:
-                dict1[i]=[1]
-            else:
-                dict1[i]+=[1]
-        for j in t:
-            if j not in dict2:
-                dict2[j]=[1]
-            else:
-                dict2[j]+=[1]
-        if dict1==dict2:
-            return True
-        else:
-            return False    
+        # for i in s:
+        #     if i not in dict1:
+        #         dict1[i]=[1]
+        #     else:
+        #         dict1[i]+=[1]
+        # for j in t:
+        #     if j not in dict2:
+        #         dict2[j]=[1]
+        #     else:
+        #         dict2[j]+=[1]
+        # if dict1==dict2:
+        #     return True
+        # else:
+        #     return False    
+        if len(s) != len(t):
+            return False
+        for i in set(s):
+            if t.count(i)!=s.count(i):
+                return False
+        return True
